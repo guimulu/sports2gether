560a59f8351134aaac8672ed6c034001b16c9c84
Guilherme Muller <guilherme.muller@sistemairriga.com.br>
2019-09-28 07:51:01 -0300

    cadastro de eventos

--- src/pages/MarkerInfoWindow/index.js
+++ src/pages/MarkerInfoWindow/index.js
@@ -123,6 +123,132 @@ const Marker = props => {
   );
 };
 
+const Marker2 = props => {
+  const markerStyle = {
+    height: '10px',
+    width: '10px',
+    backgroundColor: 'white',
+    backgroundSize: 'cover',
+    cursor: 'pointer',
+    zIndex: 10,
+  };
+
+  return (
+    <>
+      <div style={markerStyle} />
+      {props.show && <InfoWindow2 props={props} />}
+    </>
+  );
+};
+
+const InfoWindow2 = props => {
+  console.log(props)
+  const infoWindowStyle = {
+    position: 'relative',
+    bottom: 150,
+    left: '-45px',
+    width: 220,
+    backgroundColor: '#5fff5c',
+    boxShadow: '0 2px 7px 1px rgba(0, 0, 0, 0.3)',
+    borderRadius: '10%',
+    padding: 10,
+    fontSize: 14,
+    zIndex: 100,
+  };
+
+  const buttonStyle = {
+    borderRadius: '15px',
+    width: '130px',
+    height: '30px',
+    borderWidth: '0px',
+    backgroundColor: '#4a82ff',
+    color: 'white',
+    fontSize: '15px',
+    fontWeight: '700',
+    marginTop: '15px',
+  };
+
+  return (
+    <div style={infoWindowStyle}>
+      <div style={{ fontSize: 20, fontWeight: 'bold', color: '#656565' }}>
+        <span>Evento</span><input id="evento"></input>
+        <span style={{ float: 'right' }}>x</span>
+      </div>
+      <div style={{ fontSize: 15 }}>
+        <img
+          src={Distancia}
+          alt="Distancia"
+          width="25"
+          style={{ verticalAlign: 'middle' }}
+        />
+        <span
+          style={{ color: 'grey', verticalAlign: 'middle', marginLeft: '10px' }}
+        >
+          Distancia:
+        </span>
+        <input id="distancia"></input>
+      </div>
+      <div style={{ fontSize: 15 }}>
+        <img
+          src={Calendario}
+          alt="Calendario"
+          width="25"
+          style={{ verticalAlign: 'middle' }}
+        />
+        <span
+          style={{ color: 'grey', verticalAlign: 'middle', marginLeft: '10px' }}
+        >
+          Data e Hora:
+        </span>
+        <input id="calendario"></input>
+      </div>
+      <div style={{ fontSize: 15 }}>
+        <img
+          src={Calendario}
+          alt="Calendario"
+          width="25"
+          style={{ verticalAlign: 'middle' }}
+        />
+        <span
+          style={{ color: 'grey', verticalAlign: 'middle', marginLeft: '10px' }}
+        >
+          Esporte:
+        </span>
+        <select id="esporte">
+          <option value="1">Ciclismo</option>
+          <option value="2">Corrida</option>
+        </select>
+      </div>
+      <div style={{ textAlign: 'center' }}>
+        <button onClick={() => cadastrarEvento(1, props.props.lat, props.props.lng)} style={buttonStyle}>
+          Criar Evento
+        </button>
+      </div>
+    </div>
+  );
+};
+
+
+function cadastrarEvento (sportType, lat, lng) {
+  api
+  .post('/events', {
+    name: document.getElementById('evento').value,
+    user_id: userLogged.id,
+    sport_type_id: sportType,
+    lat,
+    lng,
+    period: document.getElementById('calendario').value,
+    distance: document.getElementById('distancia').value,
+  })
+  .then(function(response) {
+    console.log(response);
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+
+}
+
 class MarkerInfoWindow extends Component {
   constructor(props) {
     super(props);
@@ -131,6 +257,9 @@ class MarkerInfoWindow extends Component {
       places: [],
       users: [],
       userLogged: null,
+      lat: null,
+      lng: null,
+      showInfoWindow: false
     };
   }
 
@@ -156,13 +285,24 @@ class MarkerInfoWindow extends Component {
 
   // onChildClick callback can take two arguments: key and childProps
   onChildClickCallback = key => {
-    this.setState(teste => {
-      const index = teste.places.findIndex(
-        e => parseInt(e.id) === parseInt(key)
-      );
-      teste.places[index].show = !teste.places[index].show; // eslint-disable-line no-param-reassign
-      return { places: teste.places };
-    });
+    if (key != 6527) {
+      this.setState(place => {
+        const index = place.places.findIndex(
+          e => parseInt(e.id) === parseInt(key)
+        );
+        place.places[index].show = !place.places[index].show; // eslint-disable-line no-param-reassign
+        return { places: place.places };
+      });
+    } else {
+      this.setState(state => {
+        return { places: state.places, showInfoWindow: false };
+      });
+    }
+  };
+
+  _onClick = ({lat, lng}) => {
+    console.log(lat, lng);
+    this.setState({lat, lng, showInfoWindow: true})
   };
 
   render() {
@@ -175,6 +315,7 @@ class MarkerInfoWindow extends Component {
             defaultCenter={SANTA_MARIA_CENTER}
             bootstrapURLKeys={{ key: process.env.REACT_APP_MAP_KEY }}
             onChildClick={this.onChildClickCallback}
+            onClick={this._onClick}
           >
             {places.map(place => (
               <Marker
@@ -186,6 +327,15 @@ class MarkerInfoWindow extends Component {
                 sport_type_id={place.sport_type_id}
               />
             ))}
+            { this.state.showInfoWindow && (
+              <Marker2
+                key={6527}
+                lat={this.state.lat}
+                lng={this.state.lng}
+                show={this.state.showInfoWindow}
+              />
+            )
+            }
           </GoogleMap>
         )}
         <List>
